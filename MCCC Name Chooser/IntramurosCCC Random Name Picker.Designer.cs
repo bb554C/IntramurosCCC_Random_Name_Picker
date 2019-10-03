1e37314006f220cb8baac4472ecf2f4e55f15947
Baird Rouan S. Buenaventura <bairdbuenaventura@gmail.com>
2019-10-03 16:32:40 +0800

    Add files via upload

---
+++ MCCC Name Chooser/IntramurosCCC Random Name Picker.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace MCCC_Name_Chooser
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonStartRoll = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Location = new System.Drawing.Point(503, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(285, 404);
+            this.listBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(331, 69);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(80, 40);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "ADD";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.AddButton_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(417, 69);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(80, 40);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "DELETE";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.DeleteButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(134, 41);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(363, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // buttonStartRoll
+            // 
+            this.buttonStartRoll.BackColor = System.Drawing.Color.Lime;
+            this.buttonStartRoll.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.buttonStartRoll.Location = new System.Drawing.Point(134, 69);
+            this.buttonStartRoll.Name = "buttonStartRoll";
+            this.buttonStartRoll.Size = new System.Drawing.Size(191, 53);
+            this.buttonStartRoll.TabIndex = 4;
+            this.buttonStartRoll.Text = "START ROLL";
+            this.buttonStartRoll.UseVisualStyleBackColor = false;
+            this.buttonStartRoll.Click += new System.EventHandler(this.ButtonStartRoll_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Lime;
+            this.label1.Location = new System.Drawing.Point(22, 176);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(205, 29);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "TESTTESTTEST";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(21, 219);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(303, 32);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "TESTTESTTESTTEST";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(27, 300);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(167, 138);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(220, 300);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(251, 138);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonStartRoll);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Main";
+            this.Text = "Random Name Picker";
+            this.Load += new System.EventHandler(this.Main_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonStartRoll;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ MCCC Name Chooser/IntramurosCCC Random Name Picker.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace MCCC_Name_Chooser
+{
+    public partial class Main : Form
+    {
+        int count=0;
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            var reader = new StreamReader(File.OpenRead("NameList.txt"));
+            while (!reader.EndOfStream)
+            {
+                count++;
+                var line = reader.ReadLine();
+                listBox1.Items.Add(line);
+            }
+            reader.Close();
+            listBox1.Sorted = true;
+            label1.Text = "";
+            label2.Text = "";
+        }
+
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+            int WriteCount = 0;
+            if(textBox1.Text!="")
+            {
+                count++;
+                listBox1.Items.Add(textBox1.Text);
+            }
+            var writer = new StreamWriter(File.OpenWrite("NameList.txt"));
+            while(WriteCount<count)
+            {
+                listBox1.SetSelected(WriteCount, true);
+                writer.WriteLine(listBox1.SelectedItem.ToString());
+                WriteCount++;
+            }
+            writer.Close();
+            listBox1.Sorted = true;
+            
+        }
+
+        private void DeleteButton_Click(object sender, EventArgs e)
+        {
+            count--;
+            int WriteCount = 0;
+            int index;
+            index = listBox1.SelectedIndex;
+            listBox1.Items.RemoveAt(index);
+            if(index!=-1)
+            {
+                File.WriteAllText("NameList.txt", String.Empty);
+                var writer = new StreamWriter(File.OpenWrite("NameList.txt"));
+
+                while (WriteCount < count)
+                {
+                    listBox1.SetSelected(WriteCount, true);
+                    writer.WriteLine(listBox1.SelectedItem.ToString());
+                    WriteCount++;
+                }
+                writer.Close();
+            }
+            listBox1.Sorted = true;
+        }
+
+        private void ButtonStartRoll_Click(object sender, EventArgs e)
+        {
+            label1.Text = "WINNER";
+            int loop = 0;
+            int randNum;
+            Random rand = new Random();
+            loop = 0;
+            while (loop != 30)
+            {
+                randNum = rand.Next(0, count);
+                listBox1.SelectedIndex = randNum;
+                loop++;
+                Thread.Sleep(300);
+            }
+            loop = 0;
+            while (loop != 40)
+            {
+                randNum = rand.Next(0, count);
+                listBox1.SelectedIndex = randNum;
+                loop++;
+                Thread.Sleep(200);
+            }
+            loop = 0;
+            while (loop != 50)
+            {
+                randNum = rand.Next(0, count);
+                listBox1.SelectedIndex = randNum;
+                loop++;
+                Thread.Sleep(100);
+            }
+            loop = 0;
+            while (loop != 60)
+            {
+                randNum = rand.Next(0, count);
+                listBox1.SelectedIndex = randNum;
+                loop++;
+                Thread.Sleep(50);
+            }
+            label2.Text = listBox1.SelectedItem.ToString();
+
+        }
+    }
+}
